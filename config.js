412f5bf7d8c80ea2b67a10579ec5c82549c1be66
Tim Wright <mithgirw@gmail.com>
2016-09-08 16:49:37 -0600

    typo

--- config.js
+++ config.js
@@ -9,7 +9,6 @@ config.mongoURI = {
 
 module.exports = config;*/
 
-
 exports.MONGODB_URI= 'mongodb://heroku_724f643s:5g60oejh5vmrdchk62of122vnl@ds013966.mlab.com:13966/heroku_724f643s'
 //process.env.PORT lets the hosting service like heroku set the port otherwise it uses 8080 locally
 exports.PORT = process.env.PORT || 8080;

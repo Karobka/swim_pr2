66198188d1783584ea66f7558caaf74783b74f49
Tim Wright <mithgirw@gmail.com>
2016-09-08 16:48:22 -0600

    added some comments

--- config.js
+++ config.js
@@ -1,4 +1,15 @@
 exports.DATABASE_URL = 'mongodb://localhost/swim_pr2'
+
+/*var config = {};
+
+config.mongoURI = {
+  development: 'mongodb://heroku_724f643s:5g60oejh5vmrdchk62of122vnl@ds013966.mlab.com:13966/heroku_724f643s',
+  test: 'mongodb://localhost/swim_pr2'
+};
+
+module.exports = config;*/
+
+
 exports.MONGODB_URI= 'mongodb://heroku_724f643s:5g60oejh5vmrdchk62of122vnl@ds013966.mlab.com:13966/heroku_724f643s'
 //process.env.PORT lets the hosting service like heroku set the port otherwise it uses 8080 locally
 exports.PORT = process.env.PORT || 8080;
